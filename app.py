0b4a1c34353bbb526b6c76601a1b2d7b13a81a88
boonjun123654 <boonjunlee@gmail.com>
2025-06-30 21:40:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, url_for
+from flask import Flask, render_template, request, redirect, session, url_for, flash, get_flashed_messages
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 
